b0366fb1aa781fe422d197a8373dd70353945c60
Benjamin Bolin <benjamin.bolin@elev.ga.ntig.se>
2026-05-18 18:00:47 +0200

    Testar main

--- src/main/java/jk/Main.java
+++ src/main/java/jk/Main.java
@@ -1,10 +1,24 @@
 package jk;
 
-import com.google.gson.Gson;
-
+/**
+ * Benjamin Bolin
+ */
 public class Main {
     public static void main(String[] args) {
-        String baseURL = "http://localhost:3000/";
-        Gson gson = new Gson();
+        
+        System.out.println("--- Testar att skapa objekt ---");
+
+        // Skapa bok (id, title, isAvailable, author, genre, pages)
+        Book minBok = new Book("1", "Ringo Can Read", true, "Daniel Harper", "Drama", 250);
+
+        // Skapa tidning (id, title, isAvailable, issueNumber, category, publishedYear)
+        Magazine minTidning = new Magazine("2", "Tech World", true, 12, "Technology", 2025);
+
+        // Anropa objekten 
+        System.out.println("\n=== om boken ===");
+        System.out.println(minBok.getInfo());
+
+        System.out.println("\n=== om tidningen ===");
+        System.out.println(minTidning.getInfo());
     }
 }
