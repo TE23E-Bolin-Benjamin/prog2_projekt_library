44d9fc7d2eedaf3ff2821b2b282224266519f7a7
Benjamin Bolin <benjamin.bolin@elev.ga.ntig.se>
2026-05-21 20:49:48 +0200

    skapar avstängda användare, tar bort dem från suspendedUsers. Tar bort aktiva användare.  Inte helt klar än

--- src/main/java/jk/LibraryManager.java
+++ src/main/java/jk/LibraryManager.java
@@ -5,12 +5,15 @@ import com.google.gson.reflect.TypeToken;
 import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
 import java.lang.reflect.Type;
+//import java.security.cert.CertPathValidatorException.Reason;
 import java.util.ArrayList;
 import java.util.Collections;
 
+
 /**
  * Benjamin Bolin
- * LibraryManager hanterar alla REST-API (GET, POST, DELETE) samt sök- och sorteringsalgoritmer.
+ * LibraryManager hanterar alla REST-API (GET, POST, DELETE) samt sök- och
+ * sorteringsalgoritmer.
  */
 public class LibraryManager {
     // Aggregat-listor enligt mitt klassdiagram
@@ -26,34 +29,45 @@ public class LibraryManager {
         try {
             HttpResponse<String> response = Unirest.get(baseURL + "books").asString();
             if (response.getStatus() == 200) {
-                Type listType = new TypeToken<ArrayList<Book>>(){}.getType();
+                Type listType = new TypeToken<ArrayList<Book>>() {
+                }.getType();
                 ArrayList<Book> serverBooks = gson.fromJson(response.getBody(), listType);
                 mediaList.addAll(serverBooks);
                 System.out.println("Lyckades hämta " + serverBooks.size() + " böcker!");
             }
-        } catch (Exception e) { System.out.println("Kunde inte hämta böcker."); }
+        } catch (Exception e) {
+            System.out.println("Kunde inte hämta böcker.");
+        }
     }
 
     public void fetchMagazines() {
         try {
             HttpResponse<String> response = Unirest.get(baseURL + "magazines").asString();
             if (response.getStatus() == 200) {
-                Type listType = new TypeToken<ArrayList<Magazine>>(){}.getType();
+                Type listType = new TypeToken<ArrayList<Magazine>>() {
+                }.getType();
                 ArrayList<Magazine> serverMags = gson.fromJson(response.getBody(), listType);
                 mediaList.addAll(serverMags);
                 System.out.println("Lyckades hämta " + serverMags.size() + " tidningar!");
             }
-        } catch (Exception e) { System.out.println("Kunde inte hämta tidningar."); }
+        } catch (Exception e) {
+            System.out.println("Kunde inte hämta tidningar.");
+        }
     }
+
     // både användare och suspenderade användare hämtas via denna method
     public void fetchUsersAndSuspended() {
         try {
             String uBody = Unirest.get(baseURL + "users").asString().getBody();
-            userList = gson.fromJson(uBody, new TypeToken<ArrayList<User>>(){}.getType());
+            userList = gson.fromJson(uBody, new TypeToken<ArrayList<User>>() {
+            }.getType());
 
             String sBody = Unirest.get(baseURL + "suspendedUsers").asString().getBody();
-            suspendedList = gson.fromJson(sBody, new TypeToken<ArrayList<SuspendedUser>>(){}.getType());
-        } catch (Exception e) { System.out.println("Kunde inte hämta användardata."); }
+            suspendedList = gson.fromJson(sBody, new TypeToken<ArrayList<SuspendedUser>>() {
+            }.getType());
+        } catch (Exception e) {
+            System.out.println("Kunde inte hämta användardata.");
+        }
     }
 
     // --- C-krav: Kommunikation via POST ---
@@ -63,12 +77,14 @@ public class LibraryManager {
                     .header("Content-Type", "application/json").body(gson.toJson(m)).asString();
             if (response.getStatus() == 201) {
                 mediaList.add(m);
-                System.out.println("Feedback: Resursen '" + m.getTitle() + "' har sparats på servern.");
+                System.out.println("Feedback: Media '" + m.getTitle() + "' har sparats på servern.");
             }
-        } catch (Exception e) { System.out.println("Fel vid POST till servern."); }
+        } catch (Exception e) {
+            System.out.println("Fel vid POST till servern.");
+        }
     }
 
-    public void addUserToServer(User u) {
+    public void addUserToServer(User u, SuspendedUser su) {
         try {
             HttpResponse<String> response = Unirest.post(baseURL + "users")
                     .header("Content-Type", "application/json").body(gson.toJson(u)).asString();
@@ -76,14 +92,44 @@ public class LibraryManager {
                 userList.add(u);
                 System.out.println("Feedback: Användaren '" + u.getName() + "' har sparats på servern.");
             }
-        } catch (Exception e) { System.out.println("Fel vid skapande av användare på servern."); }
+        } catch (Exception e) {
+            System.out.println("Fel vid skapande av användare på servern.");
+        }
+
+        if (u != null && su != null) {
+            addSuspendedUserToServer(u, su);
+        }
     }
 
-    // Kommunikation med servern via DELETE 
+    public void addUserToSuspendedViaUsername(String name, String uuid, String reason) {    
+        User u = getCustomerByName(name);            
+        addSuspendedUserToServer(u, new SuspendedUser(uuid, u.getId(), reason));
+   }
+
+
+    // metod som anropas från addUserToServer eller addUserToSuspendedViaUsername
+    public void addSuspendedUserToServer(User u, SuspendedUser su) {
+
+        try {
+            HttpResponse<String> response = Unirest.post(baseURL + "suspendedUsers")
+                    .header("Content-Type", "application/json").body(gson.toJson(su)).asString();
+1            if (response.getStatus() == 201) {
+                suspendedList.add(su);
+                System.out.println("Feedback: Användaren '" + u.getName() + "' har suspenderats på servern.");
+            }
+        } catch (Exception e) {
+            System.out.println("Fel vid suspendering av användare på servern.");
+        }
+    }
+
+    // Kommunikation med servern via DELETE
     public void removeMediaFromServer(String title) {
         Media target = null;
         for (Media m : mediaList) {
-            if (m.getTitle().equalsIgnoreCase(title)) { target = m; break; }
+            if (m.getTitle().equalsIgnoreCase(title)) {
+                target = m;
+                break;
+            }
         }
         if (target != null) {
             String endpoint = (target instanceof Book) ? "books/" : "magazines/";
@@ -92,7 +138,63 @@ public class LibraryManager {
                 mediaList.remove(target);
                 System.out.println("Feedback: Raderade '" + title + "' från servern.");
             }
-        } else { System.out.println("Feedback: Kunde inte hitta media."); }
+        } else {
+            System.out.println("Feedback: Kunde inte hitta media.");
+        }
+    }
+
+    // Kommunikation med servern via DELETE
+    // denna method används om man vet userid
+    public void removeSuspended(String userId) {
+        SuspendedUser target = null;
+        for (SuspendedUser su : suspendedList) {
+            if (su.getUserId().equals(userId)) {
+                target = su;
+                break;
+            }
+        }
+        if (target != null) {
+            int status = Unirest.delete(baseURL + "suspendedUsers" + target.getId()).asString().getStatus();
+            if (status == 200 || status == 204) {
+                suspendedList.remove(target);
+                System.out.println("Feedback: '" + userId + "' är nu aktiverad.");
+            }
+        } else {
+            System.out.println("Feedback: Kunde inte hitta suspendedUser.");
+        }
+    }
+
+    // om man vet namn på användaren som är suspenderad så kan man aktivera direkt
+    public void removeSuspendedUserViaUsername(String name) {
+        User u = getCustomerByName(name);
+        if (u != null) {
+            SuspendedUser target = getSuspendedCustomer(u.getId());
+            if (target != null) {
+                int status = Unirest.delete(baseURL + "suspendedUsers" + target.getId()).asString().getStatus();
+                if (status == 200 || status == 204) {
+                    suspendedList.remove(target);
+                    System.out.println("Feedback: '" + name + "' är nu aktiverad.");
+                }
+            } else {
+                System.out.println("Feedback: Kunde inte hitta suspenderad användare.");
+            }
+        } else {
+            System.out.println("Feedback: removeSuspendedUserViaUsername kunde inte hitta användare.");
+        }
+
+    }
+
+    public void removeUser(String user) {
+        User target = getCustomerByName(user);
+        if (target != null) {
+            int status = Unirest.delete(baseURL + "users" + target.getId()).asString().getStatus();
+            if (status == 200 || status == 204) {
+                userList.remove(target);
+                System.out.println("Feedback: '" + user + "' är nu borttagen.");
+            }
+        } else {
+            System.out.println("Feedback: Kunde inte hitta användare.");
+        }
     }
 
     // Algoritmer för sökning och sortering
@@ -101,7 +203,8 @@ public class LibraryManager {
             System.out.println("Samlingen är tom lokalt. Hämta data från servern först.");
         } else {
             Collections.sort(mediaList); // Sorterar samlingen automatiskt via Comparable
-            for (Media m : mediaList) System.out.println(m.getInfo());
+            for (Media m : mediaList)
+                System.out.println(m.getInfo());
         }
     }
 
@@ -110,7 +213,8 @@ public class LibraryManager {
             System.out.println("Inga kunder inladdade.");
         } else {
             Collections.sort(userList); // Sorterar kunder automatiskt via Comparable på namn
-            for (User u : userList) System.out.println(u.getInfo());
+            for (User u : userList)
+                System.out.println(u.getInfo());
         }
     }
 
@@ -122,49 +226,81 @@ public class LibraryManager {
                 hittad = true;
             }
         }
-        if (!hittad) System.out.println("Ingen media matchade sökningen.");
+        if (!hittad)
+            System.out.println("Ingen media matchade sökningen.");
     }
 
-    public void findCustomerByEmail(String email) {
-        boolean hittad = true;
+    // methoden visar alla användare som matchar
+    public void findCustomerByNameAndEmail(String nameAndEmail) {
+        boolean found = false;
         for (User u : userList) {
-            if (u.getEmail().equalsIgnoreCase(email)) {
-                System.out.println(u.getInfo());
-                // Kontrollerar direkt om kunden är spärrad eller inte (C-krav)
-                boolean suspended = true;
-                for (SuspendedUser su : suspendedList) {
-                    if (su.getUserId().equals(u.getId())) { suspended = false; break; }
-                }
-                System.out.println("Lånestatus: " + (suspended ? "FÅR LÅNA" : "SPÄRRAD (Får ej låna!)"));
-                return;
+            if (u.getEmail().equalsIgnoreCase(nameAndEmail) || u.getName().equalsIgnoreCase(nameAndEmail)) {
+                System.out.println(u.getInfo()); // kan inte visa reason pga modellering och vill inte söka i suspended
+                found = true; // ingen breake visar alla användare
             }
         }
-         if (!hittad) System.out.println("Ingen matchade sökningen.");
+        if (!found)
+            System.out.println("Ingen användare matchade sökningen.");
     }
-        
+
     public void findAvailableCustomers() {
+        boolean showError = true;
         for (User u : userList) {
-                boolean suspended = false;
-                for (SuspendedUser su : suspendedList) {
-                    if (su.getUserId().equals(u.getId())) { suspended = true; break; }
+            boolean suspended = false;
+            for (SuspendedUser su : suspendedList) {
+                if (su.getUserId().equals(u.getId())) {
+                    suspended = true;
+                    showError = false;
+                    break;
                 }
-                if (!suspended) {System.out.println(u.getInfo());}
             }
-        
-        System.out.println("Hittade ingen kund med den e-postadressen.");
+            if (!suspended) {
+                System.out.println(u.getInfo());
+            }
+        }
+
+        if (showError) {
+            System.out.println("Hittade ingen Kund");
+        }
     }
 
-        public void findSuspendedCustomers() {
+    public void findSuspendedCustomers() {
+        boolean showError = true;
         for (User u : userList) {
-                boolean suspended = false;
-                for (SuspendedUser su : suspendedList) {
-                    if (su.getUserId().equals(u.getId())) { suspended = true; break; }
+            for (SuspendedUser su : suspendedList) {
+                if (su.getUserId().equals(u.getId())) {
+                    System.out.println(u.getInfo() + ", Anledning " + su.getReason());
+                    showError = false;
+                    break;
                 }
-                if (suspended) {System.out.println(u.getInfo());}
             }
-        
-        System.out.println("Hittade ingen kund med den e-postadressen.");
+        }
+        if (showError) {
+            System.out.println("Hittade ingen Suspenderad kund");
+            return; // Behöver inte loopa mera
+        }
+
     }
 
+    // funktion så att man hittar användare så att den kan tas bort
+    public User getCustomerByName(String name) {
+        for (User u : userList) {
+            if (u.getName().equalsIgnoreCase(name)) {
+                return u; // risk för dubletter första tas bort iaf
+            }
+        }
+        System.out.println("Ingen användare matchade sökningen.");
+        return null;
+    }
+
+    // för att kunna ta bort avstängningen behöver jag hämta suspendeduser
+    public SuspendedUser getSuspendedCustomer(String userId) {
+        for (SuspendedUser su : suspendedList) {
+            if (su.getUserId().equals(userId))
+                return su;
+        }
+        System.out.println("Ingen supsenderad användare matchade sökningen.");
+        return null;
+    }
 
 }
--- src/main/java/jk/Main.java
+++ src/main/java/jk/Main.java
@@ -3,7 +3,6 @@ package jk;
 import java.util.Scanner;
 import java.util.UUID;
 
-
 /**
  * Benjamin Bolin
  * Libsys startas via Main klassen som visar menyer i flera nivåer
@@ -16,38 +15,35 @@ public class Main {
         boolean run = true;
         manager.fetchUsersAndSuspended(); // Hämtar kunddata vid uppstart
 
-        System.out.println("Välkommen till LibSys!");  
+        System.out.println("Välkommen till LibSys!");
 
         while (run) {
             System.out.println("""
-                    
+
                     === HUVUDMENY ===
                     1. Hämta & Visa (Undermeny)
                     2. Skapa & Lägg till (Undermeny)
                     3. Sökfunktioner (Undermeny)
-                    4. Ta bort media från servern
+                    4. Ta bort (Undermeny)
                     5. Avsluta LibSys
                     """);
             System.out.print("Välj ett alternativ (1-5) och tryck på Enter: ");
             String input = scanner.nextLine();
             int choice = 0;
 
-             // Try-catch för att förhindra krasch vid felaktig menyinmatning 
+            // Try-catch för att förhindra krasch vid felaktig menyinmatning
             try {
                 choice = Integer.parseInt(input);
             } catch (NumberFormatException e) {
-                System.out.println("Nej, det där är inte en siffra. Försök igen!"); 
+                System.out.println("Nej, det där är inte en siffra. Försök igen!");
                 continue;
             }
 
             switch (choice) {
-                case 1 -> fetchMenu(); 
-                case 2 -> createMenu(); 
-                case 3 -> searchMenu(); 
-                case 4 -> {
-                    System.out.print("Ange exakt titel på den media du vill ta bort från servern: ");
-                    manager.removeMediaFromServer(scanner.nextLine());
-                }
+                case 1 -> fetchMenu();
+                case 2 -> createMenu();
+                case 3 -> searchMenu();
+                case 4 -> deletehMenu();
                 case 5 -> {
                     System.out.println("Avslutar programmet. Hejdå!");
                     run = false;
@@ -58,7 +54,6 @@ public class Main {
         }
     }
 
-    
     private static void fetchMenu() {
         System.out.println("""
                 \n--- UNDERMENY: HÄMTA & VISA ---
@@ -66,18 +61,26 @@ public class Main {
                 2. Hämta tidningar från Servern
                 3. Skriv ut all hämtad media (Sorterad)
                 4. Skriv ut alla kunder (Sorterat på Namn)
+                5. Skriv ut alla suspenderade kunder
                 """);
         System.out.print("Val: ");
         try {
             int val = Integer.parseInt(scanner.nextLine());
-            if (val == 1) manager.fetchBooks();
-            else if (val == 2) manager.fetchMagazines();
-            else if (val == 3) manager.printMediaSorted();
-            else if (val == 4) manager.printUsersSorted();
-   
-        } catch (Exception e) 
-        { 
-            System.out.println("Felaktigt format."); 
+            if (val == 1)
+                manager.fetchBooks();
+            else if (val == 2)
+                manager.fetchMagazines();
+            else if (val == 3)
+                manager.printMediaSorted();
+            else if (val == 4)
+                manager.printUsersSorted();
+            else if (val == 5) {
+                System.out.println("Kunder som inte får låna är: ");
+                manager.findSuspendedCustomers();
+            }
+
+        } catch (Exception e) {
+            System.out.println("Felaktigt format.");
 
         }
     }
@@ -89,30 +92,67 @@ public class Main {
                 1. Ny Bok
                 2. Ny Tidning
                 3. Ny Användare
+                4. Ny suspended användare
+                5. Suspendera en befintlig användare
+                6. Aktivera suspenderad användare
                 """);
-        System.out.print("Val (1-3): ");
+        System.out.print("Val (1-6): ");
         try {
             int val = Integer.parseInt(scanner.nextLine());
             switch (val) {
                 case 1 -> {
-                    System.out.print("Ange titel: "); String t = scanner.nextLine();
-                    System.out.print("Ange författare: "); String a = scanner.nextLine();
-                    System.out.print("Ange genre: "); String g = scanner.nextLine();
-                    System.out.print("Ange antal sidor: "); int p = Integer.parseInt(scanner.nextLine());
+                    System.out.print("Ange titel: ");
+                    String t = scanner.nextLine();
+                    System.out.print("Ange författare: ");
+                    String a = scanner.nextLine();
+                    System.out.print("Ange genre: ");
+                    String g = scanner.nextLine();
+                    System.out.print("Ange antal sidor: ");
+                    int p = Integer.parseInt(scanner.nextLine());
                     manager.addMediaToServer(new Book(UUID.randomUUID().toString(), t, true, a, g, p), "books");
                 }
                 case 2 -> {
-                    System.out.print("Ange titel: "); String t = scanner.nextLine();
-                    System.out.print("Ange kategori: "); String c = scanner.nextLine();
-                    System.out.print("Ange utgåvonummer: "); int num = Integer.parseInt(scanner.nextLine());
-                    System.out.print("Ange utgivningsår: "); int y = Integer.parseInt(scanner.nextLine());
-                    manager.addMediaToServer(new Magazine(UUID.randomUUID().toString(), t, true, num, c, y), "magazines");
+                    System.out.print("Ange titel: ");
+                    String t = scanner.nextLine();
+                    System.out.print("Ange kategori: ");
+                    String c = scanner.nextLine();
+                    System.out.print("Ange utgåvonummer: ");
+                    int num = Integer.parseInt(scanner.nextLine());
+                    System.out.print("Ange utgivningsår: ");
+                    int y = Integer.parseInt(scanner.nextLine());
+                    manager.addMediaToServer(new Magazine(UUID.randomUUID().toString(), t, true, num, c, y),
+                            "magazines");
                 }
                 case 3 -> {
-                    System.out.print("Ange kundens namn: "); String n = scanner.nextLine();
-                    System.out.print("Ange e-postadress: "); String em = scanner.nextLine();
-                    manager.addUserToServer(new User(UUID.randomUUID().toString(), n, em));
+                    System.out.print("Ange kundens namn: ");
+                    String n = scanner.nextLine();
+                    System.out.print("Ange e-postadress: ");
+                    String em = scanner.nextLine();
+                    manager.addUserToServer(new User(UUID.randomUUID().toString(), n, em), null);
                 }
+                case 4 -> {
+                    System.out.print("Ange kundens namn: ");
+                    String n = scanner.nextLine();
+                    System.out.print("Ange e-postadress: ");
+                    String em = scanner.nextLine();
+                    System.out.print("Ange anledning: ");
+                    String reason = scanner.nextLine();
+                    User u = new User(UUID.randomUUID().toString(), n, em);
+                    manager.addUserToServer(u, new SuspendedUser(UUID.randomUUID().toString(), u.getId(), reason));
+                }
+                case 5 -> {
+                    System.out.print("Ange kundens namn: ");
+                    String n = scanner.nextLine();
+                    System.out.print("Ange anledning för att suspendera: ");
+                    String reason = scanner.nextLine();                            
+                    manager.addUserToSuspendedViaUsername(n, UUID.randomUUID().toString(), reason);
+                }
+                case 6 -> {
+                    System.out.print("Ange kundens namn: ");
+                    String n = scanner.nextLine();                                        
+                    manager.removeSuspendedUserViaUsername(n);
+                }
+
                 default -> System.out.println("Ogiltigt val i undermenyn.");
             }
         } catch (Exception e) {
@@ -125,27 +165,54 @@ public class Main {
         System.out.println("""
                 \n--- UNDERMENY: SÖKFUNKTIONER ---
                 1. Sök efter bok/tidning på titel
-                2. Hitta kund med hjälp av e-postadress
+                2. Hitta kund med hjälp av namn eller e-postadress
                 3. Visa vilka som får låna
-                4. Visas suspenderade
                 """);
-        System.out.print("Val (1-4): ");
+        System.out.print("Val (1-3): ");
         try {
             int val = Integer.parseInt(scanner.nextLine());
             if (val == 1) {
                 System.out.print("Mata in sökord på titel: ");
                 manager.findBookOrMagazine(scanner.nextLine());
             } else if (val == 2) {
-                System.out.print("Mata in kundens e-post: ");
-                manager.findCustomerByEmail(scanner.nextLine());
-            }else if (val == 3) {
+                System.out.print("Mata in kundens namn eller e-post: ");
+                manager.findCustomerByNameAndEmail(scanner.nextLine());
+            } else if (val == 3) {
                 System.out.print("Kunder som får låna är: ");
                 manager.findAvailableCustomers();
-            }else if (val == 4) {
-                System.out.print("Kunder som inte får låna är: ");
-                manager.findSuspendedCustomers();
             }
+        } catch (Exception e) {
+            System.out.println("Felaktigt val.");
+        }
+    }
 
+    private static void deletehMenu() {
+        System.out.println("""
+                \n--- UNDERMENY:  ---
+                1. Ta bort Media
+                2. Aktivera avstängd användare via namn
+                3. Aktivera avstängd användare via id
+                4. Ta bort användare via namn
+                """);
+        System.out.print("Val (1-3): ");
+        try {
+            int val = Integer.parseInt(scanner.nextLine());
+            if (val == 1) {
+                System.out.print("Ange exakt titel på den media du vill ta bort från servern: ");
+                manager.removeMediaFromServer(scanner.nextLine());
+            } else if (val == 2) {
+                System.out.print("Ange kundens namn som ska aktiveras: ");
+                String n = scanner.nextLine();
+                manager.removeSuspendedUserViaUsername(n);
+            } else if (val == 3) {
+                System.out.print("Ange kundens id som ska aktiveras: ");
+                String n = scanner.nextLine();
+                manager.removeSuspended(n);
+            } else if (val == 4) {
+                System.out.print("Ange kundens namn som ska tas bort: ");
+                String n = scanner.nextLine();
+                manager.removeUser(n);
+            }
         } catch (Exception e) {
             System.out.println("Felaktigt val.");
         }
